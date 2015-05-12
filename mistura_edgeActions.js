3f9039d12f2fe13fc41a26998baf278c37c25462
eliaspol <elias.fpp@gmail.com>
2015-05-12 15:43:15 -0400

    attempt02

---
+++ mistura_edgeActions.js
@@ -0,0 +1,84 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_letter06}","mouseover",function(sym,e){sym.$("letter06").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter06")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter13}","mouseover",function(sym,e){sym.$("letter13").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter13")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${__01}","mouseover",function(sym,e){sym.$("_01").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter02}","mouseover",function(sym,e){sym.$("letter02").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter02")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter11}","mouseover",function(sym,e){sym.$("letter11").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter11")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter16}","mouseover",function(sym,e){sym.$("letter16").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter16")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter04}","mouseover",function(sym,e){sym.$("letter04").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter04")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter03}","mouseover",function(sym,e){sym.$("letter03").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter05}","mouseover",function(sym,e){sym.$("letter05").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter07}","mouseover",function(sym,e){sym.$("letter07").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter08}","mouseover",function(sym,e){sym.$("letter08").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter09}","mouseover",function(sym,e){sym.$("letter09").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter10}","mouseover",function(sym,e){sym.$("letter10").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter12}","mouseover",function(sym,e){sym.$("letter12").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter14}","mouseover",function(sym,e){sym.$("letter14").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter15}","mouseover",function(sym,e){sym.$("letter15").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Group29}","click",function(sym,e){sym.$("Cavalo")[0].play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter06}","touchstart",function(sym,e){sym.$("letter06").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter06")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${__01}","touchstart",function(sym,e){sym.$("_01").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter02}","touchstart",function(sym,e){sym.$("letter02").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter02")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter03}","touchstart",function(sym,e){sym.$("letter03").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter04}","touchstart",function(sym,e){sym.$("letter04").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter04")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter05}","touchstart",function(sym,e){sym.$("letter05").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter07}","touchstart",function(sym,e){sym.$("letter07").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter08}","touchstart",function(sym,e){sym.$("letter08").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter09}","touchstart",function(sym,e){sym.$("letter09").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter10}","touchstart",function(sym,e){sym.$("letter10").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter11}","touchstart",function(sym,e){sym.$("letter11").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter11")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter12}","touchstart",function(sym,e){sym.$("letter12").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter13}","touchstart",function(sym,e){sym.$("letter13").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter13")});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter14}","touchstart",function(sym,e){sym.$("letter14").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter15}","touchstart",function(sym,e){sym.$("letter15").draggable({revert:"invalid"});});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_letter16}","touchstart",function(sym,e){sym.$("letter16").draggable({revert:"invalid"});sym.$("wordLine").droppable({accept:sym.$("letter16")});});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+
+//=========================================================
+
+//Edge symbol: 'line'
+(function(symbolName){})("line");
+//Edge symbol end:'line'
+})(jQuery,AdobeEdge,"EDGE-82258573");
